eb3650c2e8093dbda531d8f981cf6ec9db045aa6
Siwoo <postsw7@gmail.com>
2017-02-16 18:52:04 +0900

    last merge before code-review

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const helmet = require('helmet');
 
 const searchRouter = require('./routers/search.js');
+const rankRouter = require('./routers/rank.js');
 
 const app = express();
 const router = express.Router();
@@ -21,6 +22,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // Search Router
 app.use('/api/search', searchRouter);
+app.use('/api/rank', rankRouter);
 
 // Protect XSS
 app.use(helmet());
---
+++ server/routers/rank.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const qs = require('qs');
+
+router.route('/')
+			.get(function(req, res){
+					var champ = `https://kr.api.pvp.net/api/lol/kr/v2.5/league/challenger?api_key=RGAPI-19fbe6ff-ea8e-4f8f-ba6c-61f22719ca2c`;
+					console.log('cCOMEON!!')
+					axios.get(champ)
+						.then(function(response){
+								console.log(response.data);
+								res.json(response.data);
+						})
+						.catch(function(err){
+								console.error(err);
+						})
+			})
+
+module.exports = router;
