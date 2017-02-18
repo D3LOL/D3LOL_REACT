7e27d676cf415516c05779535dd6a500862e5b84
Siwoo <postsw7@gmail.com>
2017-02-18 17:02:07 +0900

    resolve conflict on Searchcomponent

--- src/components/UserListComponent.js
+++ src/components/UserListComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, Popover } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip, Button, Modal, Popover } from 'react-bootstrap';
 import $ from 'jquery';
 import eg from 'egjs';
 
@@ -23,18 +23,27 @@ class UserListComponent extends React.Component {
 	}	
 
   render() {
-  	
+  	const tooltip = (
+    	<Tooltip id='tooltip'>
+        	랜덤유저 선택
+       </Tooltip>
+    )
 
     return (
-    	<div id="flickingtest">
-      <div className="col-md-3 userList">
-      	
+    	
+    	
+			<div className="col-md-3 userList">
+      		
       		<img src={`http://ddragon.leagueoflegends.com/cdn/7.3.3/img/profileicon/${this.props.data.profileIconId}.png`} />
+
       		<div>{ this.props.data.name }</div>
         	<div>{ this.props.data.summonerLevel }</div>
+        	<div>ID { this.props.data.id }</div>
+        	<div>최근 업데이트 { new Date(this.props.data.revisionDate).toLocaleString() }</div>
 				
 			</div>
-			</div>
+			
+			
     );
   }
 }
