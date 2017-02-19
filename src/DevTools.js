09bc47a84d9a7f8ca3c7b186e2ae56260fb63054
Wuyialex <cci45@naver.com>
2017-02-19 19:45:36 +0900

    timetravle

---
+++ src/DevTools.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import SliderMonitor from 'redux-slider-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+
+export default createDevTools(
+
+    <SliderMonitor keyboardEnabled/>
+  
+);
+
+ // <DockMonitor toggleVisibilityKey='ctrl-h'
+ //               changePositionKey='ctrl-q'
+ //               changeMonitorKey='ctrl-m'>
+ //    <LogMonitor />
+ //    <SliderMonitor keyboardEnabled/>
+ //  </DockMonitor>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import createLogger from 'redux-logger';
 import promiseMiddleware from 'redux-promise';
 import { createStore, applyMiddleware, compose } from 'redux';
 import $ from 'jquery';
-
+import DevTools from './DevTools.js'
 
 
 
@@ -18,11 +18,10 @@ const rootElement = document.getElementById('root');
 
 const loggerMiddleware = createLogger()
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(
+const store = createStore(reducers, compose(
   applyMiddleware(
   thunkMiddleware, loggerMiddleware, promiseMiddleware
-  )
+  ), DevTools.instrument()
 ));
 
 console.log(store.getState())
@@ -30,6 +29,6 @@ console.log(store.getState())
 
 
 ReactDOM.render(
-   <Provider store={store}><App store={store}/></Provider>, rootElement
+   <Provider store={store}><div><App store={store}/><DevTools /></div></Provider>, rootElement
 );
 
